f0587e614d3fddc3ae2b98396721b8ca80a525c9
zachary <zachary.campbell2006@outlook.com>
2024-02-16 23:50:53 +1100

    First commit

---
+++ beanCounter.js
@@ -0,0 +1,11 @@
+export let beanCounter = 0;
+
+export let autoBeanBirtherType1 = 0;
+
+export function addBeans(amount) {
+  beanCounter += amount;
+}
+
+export function addAutoBeanBirtherType1(amount) {
+  autoBeanBirtherType1 += amount;
+}
